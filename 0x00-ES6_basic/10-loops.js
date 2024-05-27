c4aee2edef662f3e55028414a3fa67b3b464c3d0
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-05-27 11:42:07 +0000

    9-getFullBudget.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arrayEnd = [];
-  for (const idx in array) {
+  for (const idx of array) {
     arrayEnd.push(`${appendString}${idx}`);
   }
 
