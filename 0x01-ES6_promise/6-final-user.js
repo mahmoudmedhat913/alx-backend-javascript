0875150028316222b8a75814133e0b513da60a6e
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-05-29 18:21:07 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,18 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSighup(
+export default async function handleProfileSighup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled)([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
+}
+
+/*export default async function handleProfileSighup(
   firstName,
   lastName,
   fileName,
@@ -28,4 +39,4 @@ export default async function handleProfileSighup(
   }
 
   return res;
-}
+}*/
