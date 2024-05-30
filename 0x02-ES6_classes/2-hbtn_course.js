cff8ab7e7237faec6aa97e0ee8f197de26f56955
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-05-30 14:10:12 +0000

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,4 +1,6 @@
+/*eslint-disable no-underscore-dangle*/
 export default class HolbertonCourse {
+  /*create class*/
   constructor(name, length, students) {
     this.name = name;
     this.length = length;
@@ -18,19 +20,19 @@ export default class HolbertonCourse {
 
   get length() {
     return this._length;
-  }     
-        
+  }
+
   set length(length) {
     if ((typeof length !== 'number') && !(length instanceof Number)) {
       throw new TypeError('Length must be a number');
-    }       
+    }
     this._length = length;
   }
 
   get students() {
     return this._students;
   }
-        
+
   set students(students) {
     if (!(students instanceof Array) || !students.every((s) => typeof s === 'string')) {
       throw new TypeError('Students must be an array of strings');
