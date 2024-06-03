ab807fe3fdd6270816f2f736d55909130d49767a
mahmoudmedhat913 <mahmoudmedhat913>
2024-06-03 19:12:58 +0000

    9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,7 +1,7 @@
 const groceriesList = () => {
   const map = new Map();
   map.set('Apples', 10);
-  map.set( 'Tomatoes', 10);
+  map.set('Tomatoes', 10);
   map.set('Pasta', 1);
   map.set('Rice', 1);
   map.set('Banana', 5);
