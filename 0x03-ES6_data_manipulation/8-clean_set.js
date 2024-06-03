5546289fd183336ba0402aacb23b79e0f3ab5381
mahmoudmedhat913 <mahmoudmedhat913>
2024-06-03 19:07:18 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ const cleanSet = (set, startString) => {
 
   if (startString === '' || typeof startString !== 'string') return '';
   set.forEach((s) => {
-    if (typeof s === 'string' && s.startWith(startString)) {
+    if (typeof s === 'string' && s.startsWith(startString)) {
       strings.push(s.slice(startString.length));
     }
   });
