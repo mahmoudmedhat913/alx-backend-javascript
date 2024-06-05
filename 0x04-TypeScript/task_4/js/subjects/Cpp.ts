cb69e473c73dca89788d81b9245542bb2e6a53c8
mahmoudmedhat913 <mahmoudmedhat913>
2024-06-05 11:45:21 +0000

    task4

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,19 @@
+/// <references path="Teacher.ts" />
+/// <references path="Subjects.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subject{
+
+    getRequirements = () => 'Here is the list of requirements for Cpp';
+
+    getAvailableTeacher = () => {
+      if (!this.teacher?.experienceTeachingC) return 'No available teacher';
+
+      return `Available Teacher: ${this.teacher}`;
+    }
+  }
+}
---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,18 @@
+/// <references path="Teacher.ts" />
+/// <references path="Subjects.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingReact?: number;
+  }
+
+  export class ReactClass extends Subject{
+    getRequirements = () => 'Here is the list of requirements for React';
+
+    getAvailableTeacher = () => {
+      if (!this.teacher?.experienceTeachingReact) return 'No available teacher';
+
+      return `Available Teacher: ${this.teacher}`;
+    }
+  }
+}
