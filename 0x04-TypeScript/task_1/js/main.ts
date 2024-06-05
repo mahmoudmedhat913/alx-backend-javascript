d112af30a1e8c7a1e3cad88423e218914513c46e
mahmoudmedhat913 <mahmoudmedhat913>
2024-06-05 10:30:28 +0000

    task1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,7 +11,7 @@ interface Directors extends Teacher {
   numberOfReports: number,
 }
 
-const pinTeacher = (firstName:string, lastName:string) : string => `${firstName.charAt(0)}.${lastName}`;
+const printTeacher = (firstName:string, lastName:string) : string => `${firstName.charAt(0)}.${lastName}`;
 
 class StudentClass {
   firstName: string;
