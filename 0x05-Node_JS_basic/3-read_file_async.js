a95a37e2922a1b10b1653b54c279efd3c2577f86
mahmoudmedhat913 <mahmoudmedhat913>
2024-07-23 17:29:06 +0000

    3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -36,8 +36,8 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
 	.reduce((pre, cur) => (pre || []).length + cur.length);
       console.log(`Number of students: ${totalStudents}`);
       for (const [field, group] of Object.entries(studentGroups)) {
-	const studentNames = group.map((student) => student.firstname).join(', ');
-	console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+        const studentNames = group.map((student) => student.firstname).join(', ');
+        console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
       }
       resolve(true);
     }
