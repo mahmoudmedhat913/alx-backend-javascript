4c939881740de0a6dab31c86ac0e440f16194660
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-08-20 18:38:15 +0000

    AppController.js

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -5,10 +5,10 @@ import router from './routes';
 const express = require('express');
 const app = express();
 
-// Use the routes defined
+// Use the routes defined in full_server/routes/index.js
 app.use('/', router);
 
-// Start the server
+// Start the server on port 1245
 const port = 1245;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
